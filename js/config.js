424003c0fc43629046ddbec6f4c4c203fad05c33
Rajesh Kannan <rajesh.kannan@libertymutual.com>
2019-09-27 11:15:00 -0400

    updated cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_xVCBmnjn5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '59l9q5d36o0pk5bvr21mpln5o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
